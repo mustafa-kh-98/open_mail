8d99a10a934365e01ba723b0c494a5e5c07d2928
Mustafa Khalied <162708447+mustafa-kh-98@users.noreply.github.com>
2024-09-10 21:58:42 +0400

    Update OpenMailPlugin.swift

--- ios/Classes/OpenMailPlugin.swift
+++ ios/Classes/OpenMailPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 
-public class OpenMailPlugin: NSObject, FlutterPlugin {
+public class SwiftOpenMailPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "open_mail", binaryMessenger: registrar.messenger())
     let instance = OpenMailPlugin()
